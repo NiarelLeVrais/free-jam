f1df1f69c1cbc61ec0e46e0dda5daf91c151a369
Toto <niarel.lieurain@gmail.com>
2026-06-04 10:09:27 +0200

    Tentative

--- public/script.js
+++ public/script.js
@@ -2,9 +2,12 @@ async function checkAuth() {
     const res = await fetch('/api/me')
     const data = await res.json()
 
+    document.getElementById('disconect').style.display = 'none'
+
     if (data.connected) {
         document.getElementById('name').textContent = 'Name : ' + data.name
         document.getElementById('login').style.display = 'none'
+        document.getElementById('disconect').style.display = 'true'
     } else {
         document.getElementById('name').textContent = 'Non connecté'
         document.getElementById('login').style.display = 'inline'
